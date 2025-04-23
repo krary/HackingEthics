cd57c787e08280e2a7c0619ab2442003e253843f
unknown <claudiogonzales045@gmail.com>
2025-04-23 22:53:05 +0200

    upgrade

--- COMMANDS_Window/Powershell/Process.ps1
+++ COMMANDS_Window/Powershell/Process.ps1
@@ -6,7 +6,19 @@ function pro(){
        $id =  $i.Id
        $visible =  $i.MainWindowTitle
        $file_path =  $i.MainModule.FileName
-       
+       $sospecha = $false
+       if($file_path -notmatch "C:\\Window|C:\\Program File|C:\\Users"){
+        $sospecha = $true
+       }
+       if($cpu -gt 30){
+        $sospecha = $true
+       }
+       if($visible -eq "" -and $file_path -notmatch "Window"){
+        $sospecha = $true
+       }
+       if($sospecha){
+        Write-Host "SOSPECHOSO Nombre: $nombre Titulo: $visible Path: $file_path"
+       }
     }
 }
 pro
